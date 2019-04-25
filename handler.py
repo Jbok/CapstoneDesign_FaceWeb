d92a8eaf7aa7a6299f6bce2c4475b7f2bb0e928c
Jbok <0614bok@gmail.com>
2019-04-25 07:38:44 -0700

    Move download from s3 to global space
    https://stackoverflow.com/questions/55630791/how-to-read-trained-data-file-in-s3/55781125?noredirect=1#comment98336341_55781125
    글로벌 변수로 옮겨놓으면 Inovke할 때만 다운로드 한다고합니다.

--- handler.py
+++ handler.py
@@ -5,6 +5,18 @@ import base64
 # python AWS SDK
 import boto3
 
+BUCKET_NAME = 'lucky-faceweb-2019'
+BUCKET_TRAINED_DATA_PATH = 'TrainedData/shape_predictor_68_face_landmarks.dat'
+LOCAL_IMG_PATH = '/tmp/image.png'
+LOCAL_TRAINED_DATA_PATH = '/tmp/train.dat'
+'''
+LOCAL_IMG_PATH = 'image.png'
+LOCAL_TRAINED_DATA_PATH = 'train.dat'
+'''
+
+s3_client = boto3.client('s3')
+s3_client.download_file(BUCKET_NAME, BUCKET_TRAINED_DATA_PATH, LOCAL_TRAINED_DATA_PATH)
+
 def hello(event, context):
 
     
@@ -29,30 +41,13 @@ def hello(event, context):
     }
     """
 
-def download_trained_data(bucket_name, trained_data_path, local_file_path):
-    s3_client = boto3.client('s3')
-    s3_client.download_file(bucket_name, trained_data_path, local_file_path)
-
-
 def test(event, context):
     
     img_data = json.dumps(event['body'])
 
-    print(type(img_data))
-    print(type(img_data.encode()))
-
-    BUCKET_NAME = 'lucky-faceweb-2019'
-    BUCKET_TRAINED_DATA_PATH = 'TrainedData/shape_predictor_68_face_landmarks.dat'
-    # LOCAL_IMG_PATH = '/tmp/image.png'
-    # LOCAL_TRAINED_DATA_PATH = '/tmp/train.dat'
-    LOCAL_IMG_PATH = 'image.png'
-    LOCAL_TRAINED_DATA_PATH = 'train.dat'
-
     fh = open(LOCAL_IMG_PATH, "wb")
     fh.write(base64.decodestring(img_data.encode()))
     fh.close()
-    
-    download_trained_data(BUCKET_NAME, BUCKET_TRAINED_DATA_PATH, LOCAL_TRAINED_DATA_PATH)
 
     result = facial_landmark.cal_asymmetry(LOCAL_TRAINED_DATA_PATH, LOCAL_IMG_PATH)
 
