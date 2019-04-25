7d3b23626a5cd37d56c538f266357d37a28df52d
Jbok <0614bok@gmail.com>
2019-04-25 07:07:11 -0700

    Bring Image and Trained dat from s3

--- handler.py
+++ handler.py
@@ -1,5 +1,6 @@
 import json
 import facial_landmark
+import base64
 
 # python AWS SDK
 import boto3
@@ -29,6 +30,7 @@ def hello(event, context):
     """
 
 def download_trained_data(bucket_name, trained_data_path, local_file_path):
+    s3_client = boto3.client('s3')
     s3_client.download_file(bucket_name, trained_data_path, local_file_path)
 
 
@@ -36,15 +38,27 @@ def test(event, context):
     
     img_data = json.dumps(event['body'])
 
+    print(type(img_data))
+    print(type(img_data.encode()))
+
     BUCKET_NAME = 'lucky-faceweb-2019'
     BUCKET_TRAINED_DATA_PATH = 'TrainedData/shape_predictor_68_face_landmarks.dat'
-    LOCAL_IMG_PATH = '/tmp/image.png'
-    LOCAL_TRAINED_DATA_PATH = '/tmp/train.dat'
+    # LOCAL_IMG_PATH = '/tmp/image.png'
+    # LOCAL_TRAINED_DATA_PATH = '/tmp/train.dat'
+    LOCAL_IMG_PATH = 'image.png'
+    LOCAL_TRAINED_DATA_PATH = 'train.dat'
 
     fh = open(LOCAL_IMG_PATH, "wb")
-    fh.write(img_data.decode('base64'))
+    fh.write(base64.decodestring(img_data.encode()))
     fh.close()
     
+    download_trained_data(BUCKET_NAME, BUCKET_TRAINED_DATA_PATH, LOCAL_TRAINED_DATA_PATH)
+
     result = facial_landmark.cal_asymmetry(LOCAL_TRAINED_DATA_PATH, LOCAL_IMG_PATH)
 
     print(result)
+
+
+
+
+
