3eae7211cb85eb8451ca603fb5c6f02f18baa363
Jbok <0614bok@gmail.com>
2019-05-07 05:54:08 -0700

    Add firebase function

---
+++ db_firebase.py
@@ -0,0 +1,6 @@
+# from firebase import firebase
+
+# firebase = firebase.FirebaseApplication('https://lucky-aed54.firebaseio.com', None)
+# new_user = 'Ozgur Vatansever'
+
+# result = firebase.post('/users', new_user, {'print': 'silent'}, {'X_FANCY_HEADER': 'VERY FANCY'})
