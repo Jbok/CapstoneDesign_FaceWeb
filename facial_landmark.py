4609bfd1095cb4bb5baee867c96db43de9601207
choyumin <dbals4970@ajou.ac.kr>
2019-06-06 04:23:49 -0700

    code revision

--- facial_landmark.py
+++ facial_landmark.py
@@ -1,18 +1,21 @@
-import cv2                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-import dlib
-import imutils
+# USAGE
+# python fltest.py --shape-predictor shape_predictor_68_face_landmarks.dat --image example_01.jpg 
+
+# Using Dlib & iBUG300-W dataset
+
+# import the necessary packages
 from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import dlib
+import cv2
 import math
-from enum import IntEnum
 
-class Ptr(IntEnum):
-    X = 0
-    Y = 1
-
-def cal_degrees(p1, p2):
+def cal_degrees(p1, p2,shape):
     # Calculate the degrees of two points
-    width = abs(p1[Ptr.X] - p2[Ptr.X])
-    height = abs(p1[Ptr.Y] - p2[Ptr.Y])
+    width = abs(shape[p1][0]-shape[p2][0])
+    height = abs(shape[p1][1]-shape[p2][1])
 
     if height == 0:
         return 0
@@ -20,95 +23,84 @@ def cal_degrees(p1, p2):
     if width == 0:
         return 90
 	
-    return math.degrees(math.atan(float(height)/float(width)))
-
-def cal_asymmetry(trained_data, image_path):
-    
-    # initialize dlib's face detector (HOG-based) and then create
-    # the facial landmark predictor
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor(trained_data)
-
-    # load the input image, resize it, and convert it to grayscale
-    image = cv2.imread(image_path)
-    image = imutils.resize(image, width=300)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # detect faces in the grayscale image
-    rects = detector(gray, 1)
-
-    # Make 2D array
-    points = [[0]*2 for i in range(200)]
+    return math.degrees(math.atan(float(width)/float(height)))
 
 
-    # loop over the face detections
-    for (i, rect) in enumerate(rects):
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
+def lips_average(shape):
+  return ((abs(cal_degrees(27,54,shape)-cal_degrees(27,48,shape)))+abs((cal_degrees(27,52,shape)-cal_degrees(27,50,shape))))/2
 
-        # convert dlib's rectangle to a OpenCV-style bounding box
-        # [i.e., (x, y, w, h)], then draw the face bounding box
-        (x, y, w, h) = face_utils.rect_to_bb(rect)
-        
-        # Print Rectangle 
-        # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+def nose_average(shape):
+   return (abs(cal_degrees(27,30,shape)))
 
-        # show the face number
-        # cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
-        #     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+def facialline_average(shape):
+   sum = 0
+   for temp1 in range(1,8,1):
+    sum += abs(cal_degrees(27,temp1,shape)-cal_degrees(27,16-temp1,shape))
+   return (sum/8)
 
-        # Print Rect Num
-        # print("Face #{}".format(i + 1))	
+def cal_asymmetry(rects):
 
-        # Point Number
-        point_num = 0
+   if len(rects) == 1:
+      if len(rects) > 1:
+         print("over two")
+         return -2
+         # loop over the face detections
+      for (i, rect) in enumerate(rects):
+         shape = predictor(gray, rect)
+         shape = face_utils.shape_to_np(shape)
 
-        # loop over the (x, y)-coordinates for the facial landmarks
-        # and draw them on the image
-        for (x, y) in shape:
-            points[point_num][Ptr.X] = x
-            points[point_num][Ptr.Y] = y
+      # convert dlib's rectangle to a OpenCV-style bounding box
+      # [i.e., (x, y, w, h)], then draw the face bounding box
+         (x, y, w, h) = face_utils.rect_to_bb(rect)
+      
+      # Print Rectangle 
+         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        #     # print Point
-        #     print ("p.{}".format(point),(x,y))
-            point_num = point_num + 1
+      # show the face number
+         cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        #     cv2.putText(image, "{}".format(point), (x+2,y+2),
-        #         cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 0, 255), 1)
-        #     cv2.circle(image, (x, y), 2, (0, 255, 255), -1)
+      # Print Rect Num
+       #  print("Face #{}".format(i + 1))   
 
-        #     # Print Center Line
-        #     if point == 28:
-        #         cv2.line(image, (x, image.shape[0]), (x, 0), (255, 0, 0), 1)
+      # Point Number
+         point = 0
 
-        #     # Draw Line to Jaw
-        #     if point <= 8:
-        #         cv2.line(image, (x, y), (shape[16-point][0], shape[16-point][1]), (255, 0, 0), 1)
+         for (x, y) in shape:
+            cv2.putText(image, "{}".format(point), (x+2,y+2),
+               cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 0, 255), 1)
+            cv2.circle(image, (x, y), 2, (0, 255, 255), -1)
+            point = point + 1
+
+         jaw_degrees = facialline_average(shape)
+         nose_degrees = nose_average(shape)
+         lips_degrees = lips_average(shape)
+         return {'jaw':jaw_degrees, 'nose':nose_degrees, 'lips':lips_degrees}
+   else:
+      print("zero")
+      return -1   
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--shape-predictor", required=True,
+   help="path to facial landmark predictor")
+ap.add_argument("-i", "--image", required=True,
+   help="path to input image")
+args = vars(ap.parse_args())
+
+   # initialize dlib's face detector (HOG-based) and then create
+   # the facial landmark predictor
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(args["shape_predictor"])
+
+   # load the input image, resize it, and convert it to grayscale
+image = cv2.imread(args["image"])
+   #image = imutils.resize(image, width=300)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+   # detect faces in the grayscale image
+rects = detector(gray, 1) 
+cal_asymmetry(rects)
 
 
-        #     # Draw Line to eyes
-        #     if point == 36:
-        #         cv2.line(image, (x, y), (shape[45][0], shape[45][1]), (255, 0, 0), 1)
-        #     if point == 37:
-        #         cv2.line(image, (x, y), (shape[44][0], shape[44][1]), (255, 0, 0), 1)
-        #     if point == 38:
-        #         cv2.line(image, (x, y), (shape[43][0], shape[43][1]), (255, 0, 0), 1)
-        #     if point == 39:
-        #         cv2.line(image, (x, y), (shape[42][0], shape[42][1]), (255, 0, 0), 1)
-        #     if point == 40:
-        #         cv2.line(image, (x, y), (shape[47][0], shape[47][1]), (255, 0, 0), 1)
-        #     if point == 41:
-        #         cv2.line(image, (x, y), (shape[46][0], shape[46][1]), (255, 0, 0), 1)
-
-
-
-        jaw_degrees = 90 - cal_degrees(points[9], points[28])
-				
-        eyebrow_degrees = cal_degrees(points[20], points[25])
-				
-        lips_degrees = cal_degrees(points[49], points[55])
-
-        return {'jaw':jaw_degrees, 'eye':eyebrow_degrees, 'lips':lips_degrees}
+#cv2.imshow("Output", image)
+cv2.waitKey(0)
--- fltest.py
+++ fltest.py
@@ -1,224 +1,87 @@
-# USAGE
-# python fltest.py --shape-predictor shape_predictor_68_face_landmarks.dat --image example_01.jpg 
-
-# Using Dlib & iBUG300-W dataset
-
-# import the necessary packages
-from imutils import face_utils
-import numpy as np
-import argparse
-import imutils
+import cv2                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
 import dlib
-import cv2
+import imutils
+from imutils import face_utils
 import math
+from enum import IntEnum
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=True,
-   help="path to facial landmark predictor")
-ap.add_argument("-i", "--image", required=True,
-   help="path to input image")
-args = vars(ap.parse_args())
+class Ptr(IntEnum):
+    X = 0
+    Y = 1
 
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
+def cal_degrees(p1, p2):
+    # Calculate the degrees of two points
+    width = abs(p1[Ptr.X] - p2[Ptr.X])
+    height = abs(p1[Ptr.Y] - p2[Ptr.Y])
 
-# load the input image, resize it, and convert it to grayscale
-image = cv2.imread(args["image"])
-image = imutils.resize(image, width=300)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    if height == 0:
+        return 0
+	
+    if width == 0:
+        return 90
+	
+    return math.degrees(math.atan(float(height)/float(width)))
 
-# detect faces in the grayscale image
-rects = detector(gray, 1)
+def cal_asymmetry(trained_data, image_path):
+    
+    # initialize dlib's face detector (HOG-based) and then create
+    # the facial landmark predictor
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(trained_data)
 
-# loop over the face detections
-for (i, rect) in enumerate(rects):
-   # determine the facial landmarks for the face region, then
-   # convert the facial landmark (x, y)-coordinates to a NumPy
-   # array
+    # load the input image, resize it, and convert it to grayscale
+    image = cv2.imread(image_path)
+    image = imutils.resize(image, width=300)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-   # by yumin 
-   if i >= 1:
-      print("Only one face can be detected")
-      break
+    # detect faces in the grayscale image
+    rects = detector(gray, 1)
 
-   shape = predictor(gray, rect)
-   shape = face_utils.shape_to_np(shape)
+    # Make 2D array
+    points = [[0]*2 for i in range(200)]
 
-   # convert dlib's rectangle to a OpenCV-style bounding box
-   # [i.e., (x, y, w, h)], then draw the face bounding box
-   (x, y, w, h) = face_utils.rect_to_bb(rect)
-   
-   # Print Rectangle 
-   cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-   # show the face number
-   cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
-      cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    # loop over the face detections
+    for (i, rect) in enumerate(rects):
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
 
-   # Print Rect Num
-   print("Face #{}".format(i + 1))   
+        # convert dlib's rectangle to a OpenCV-style bounding box
+        # [i.e., (x, y, w, h)], then draw the face bounding box
+        (x, y, w, h) = face_utils.rect_to_bb(rect)
+        
+        # Print Rectangle 
+        # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-   # Point Number
-   point = 0
+        # show the face number
+        # cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
+        #     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-   # loop over the (x, y)-coordinates for the facial landmarks
-   # and draw them on the image
-   for (x, y) in shape:
-      cv2.putText(image, "{}".format(point), (x+2,y+2),
-         cv2.FONT_HERSHEY_PLAIN, 0.5, (0, 0, 255), 1)
-      cv2.circle(image, (x, y), 2, (0, 255, 255), -1)
+        # Print Rect Num
+        # print("Face #{}".format(i + 1))	
 
-      
-      # Print nose-facial line distance calculation
-      if point == 30:
-         print("nose-facial line \n")
-         temp = 0
-         for temp in range(8):   
-            cv2.line(image, (x, y), (shape[temp][0], shape[temp][1]), (255, 0, 0), 1)
-            a = shape[temp][0]-x
-            b = shape[temp][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.{}".format(temp),":",result)
-            cv2.line(image, (x, y), (shape[16-temp][0], shape[16-temp][1]), (255, 0, 0), 1)
-            a = shape[16-temp][0]-x
-            b = shape[16-temp][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.{}".format(16-temp),":",result)
-            print("\n")
-         
-         
-      # Print nose-eyes distance calculation
-       # nose-front of eyes
-      if point == 27:
-            print("nose-front of eyes \n")
-            cv2.line(image, (x, y), (shape[39][0], shape[39][1]), (255, 0, 0), 1)
-            a = shape[39][0]-x
-            b = shape[39][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.39 :",result)
-            cv2.line(image, (x, y), (shape[42][0], shape[42][1]), (255, 0, 0), 1)
-            a = shape[42][0]-x
-            b = shape[42][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.42 :",result, "\n")
+        # Point Number
+        point_num = 0
 
-            cv2.line(image, (x, y), (shape[36][0], shape[36][1]), (255, 0, 0), 1)
-            a = shape[36][0]-x
-            b = shape[36][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.36 :",result)
-            cv2.line(image, (x, y), (shape[45][0], shape[45][1]), (255, 0, 0), 1)
-            a = shape[45][0]-x
-            b = shape[45][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.45 :",result)
+        # loop over the (x, y)-coordinates for the facial landmarks
+        # and draw them on the image
+        for (x, y) in shape:
+            points[point_num][Ptr.X] = x
+            points[point_num][Ptr.Y] = y
 
-      if point == 28:
-            print("nose-front of eyes \n")
-            cv2.line(image, (x, y), (shape[39][0], shape[39][1]), (255, 0, 0), 1)
-            a = shape[39][0]-x
-            b = shape[39][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.39 :",result)
-            cv2.line(image, (x, y), (shape[42][0], shape[42][1]), (255, 0, 0), 1)
-            a = shape[42][0]-x
-            b = shape[42][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.42 :",result, "\n")
-
-            cv2.line(image, (x, y), (shape[36][0], shape[36][1]), (255, 0, 0), 1)
-            a = shape[36][0]-x
-            b = shape[36][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.36 :",result)
-            cv2.line(image, (x, y), (shape[45][0], shape[45][1]), (255, 0, 0), 1)
-            a = shape[45][0]-x
-            b = shape[45][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.45 :",result)      
-         
-      if point == 29:
-            print("nose-front of eyes \n")
-            cv2.line(image, (x, y), (shape[39][0], shape[39][1]), (255, 0, 0), 1)
-            a = shape[39][0]-x
-            b = shape[39][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.39 :",result)
-            cv2.line(image, (x, y), (shape[42][0], shape[42][1]), (255, 0, 0), 1)
-            a = shape[42][0]-x
-            b = shape[42][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.42 :",result, "\n")
-
-            cv2.line(image, (x, y), (shape[36][0], shape[36][1]), (255, 0, 0), 1)
-            a = shape[36][0]-x
-            b = shape[36][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.36 :",result)
-            cv2.line(image, (x, y), (shape[45][0], shape[45][1]), (255, 0, 0), 1)
-            a = shape[45][0]-x
-            b = shape[45][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.45 :",result)
-      
-      if point == 30:
-            print("nose-front of eyes \n")
-            cv2.line(image, (x, y), (shape[39][0], shape[39][1]), (255, 0, 0), 1)
-            a = shape[39][0]-x
-            b = shape[39][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.39 :",result)
-            cv2.line(image, (x, y), (shape[42][0], shape[42][1]), (255, 0, 0), 1)
-            a = shape[42][0]-x
-            b = shape[42][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.42 :",result, "\n")
-
-            cv2.line(image, (x, y), (shape[36][0], shape[36][1]), (255, 0, 0), 1)
-            a = shape[36][0]-x
-            b = shape[36][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.36 :",result)
-            cv2.line(image, (x, y), (shape[45][0], shape[45][1]), (255, 0, 0), 1)
-            a = shape[45][0]-x
-            b = shape[45][1]-y
-            result = math.sqrt((a*a)+(b*b))
-            print("Distance between p.{}".format(point), "and p.45 :",result)
-
-      # Print nose-lip distance calculation     
-      if point == 30:
-         print("nose-lip \n")
-         cv2.line(image, (x, y), (shape[48][0], shape[48][1]), (255, 0, 0), 1)
-         a = shape[48][0]-x
-         b = shape[48][1]-y
-         result = math.sqrt((a*a)+(b*b))
-         print("Distance between p.{}".format(point), "and p.48 :",result)
-         cv2.line(image, (x, y), (shape[54][0], shape[54][1]), (255, 0, 0), 1)
-         a = shape[54][0]-x
-         b = shape[54][1]-y
-         result = math.sqrt((a*a)+(b*b))
-         print("Distance between p.{}".format(point), "and p.54 :",result)
-         cv2.line(image, (x, y), (shape[50][0], shape[50][1]), (255, 0, 0), 1)
-         a = shape[50][0]-x
-         b = shape[50][1]-y
-         result = math.sqrt((a*a)+(b*b))
-         print("Distance between p.{}".format(point), "and p.50 :",result)
-         cv2.line(image, (x, y), (shape[52][0], shape[52][1]), (255, 0, 0), 1)
-         a = shape[52][0]-x
-         b = shape[52][1]-y
-         result = math.sqrt((a*a)+(b*b))
-         print("Distance between p.{}".format(point), "and p.52 :",result)
+        #     # print Point
+        #     print ("p.{}".format(point),(x,y))
+            point_num = point_num + 1
 
 
 
-      # print Point
-     # print ("p.{}".format(point),(x,y))
-      point = point + 1
+        jaw_degrees = 90 - cal_degrees(points[9], points[28])
+				
+        eyebrow_degrees = cal_degrees(points[20], points[25])
+				
+        lips_degrees = cal_degrees(points[49], points[55])
 
-
-# show the output image with the face detections + facial landmarks
-cv2.imshow("Output", image)
-cv2.waitKey(0)
+        return {'jaw':jaw_degrees, 'eye':eyebrow_degrees, 'lips':lips_degrees}
